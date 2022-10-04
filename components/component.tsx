8d1d9e8179e2435307b94437a56d8d8e5e27ad2e
Zach Krzyzanowski <zkrzyz@gmail.com>
2022-10-04 12:01:56 -0400

    failing tests

--- components/component.tsx
+++ components/component.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function SomeComponent() {
-  return <h2>some component</h2>;
+  return (
+    <>
+      <h2>some component</h2>
+      <span>some text</span>
+    </>
+  );
 }
